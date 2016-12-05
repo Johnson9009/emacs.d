23063d765a5762fa419ed4a5a941236da80b7a10
Johnson Zhang <Johnson9009@163.com>
2016-12-05 23:25:54 +0800

    Chang select key binding of company-mode from 'M-n M-p' to 'C-n C-p'.

--- config_els/init-keybindings.el
+++ config_els/init-keybindings.el
@@ -16,4 +16,11 @@
 (with-eval-after-load 'dired
   (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
 
+;;Use C-n C-p to select items in company-mode
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-keybindings)
