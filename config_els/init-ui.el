60974a5037766fb4515b3f28583d4e183e83d03d
Johnson Zhang <Johnson9009@163.com>
2016-12-23 01:59:01 -0800

    Show column of current curser.

--- config_els/init-ui.el
+++ config_els/init-ui.el
@@ -2,6 +2,7 @@
 (setq inhibit-splash-screen 1)
 (global-hl-line-mode 1)
 (global-linum-mode 1)
+(setq column-number-mode 1)
 (setq-default cursor-type 'bar)
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 
